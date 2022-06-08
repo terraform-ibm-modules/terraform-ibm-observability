ee8a4563ad58d71c86705eb6b444eb6eb2f74dd8
Todd Giguere <todd.giguere@ibm.com>
2022-06-08 17:34:09 -0400

    fix: pre-commit fixed outputs for previous name changes
    Needed fixes for a previous commit where module names were slightly changed.
    Forgot to change references to new module names in outputs.

--- modules/monitoring-sysdig/outputs.tf
+++ modules/monitoring-sysdig/outputs.tf
@@ -15,15 +15,15 @@ output "guid" {
 
 output "key_id" {
   description = "ID of the sysdig instance key"
-  value       = concat(ibm_resource_key.sysdigKey.*.id, [""])[0]
+  value       = concat(ibm_resource_key.sysdig_key.*.id, [""])[0]
 }
 
 output "key_guid" {
   description = "sysdig key guid"
-  value       = concat(ibm_resource_key.sysdigKey.*.guid, [""])[0]
+  value       = concat(ibm_resource_key.sysdig_key.*.guid, [""])[0]
 }
 
 output "key_credentials" {
   description = "sysdig key credentials"
-  value       = concat(ibm_resource_key.sysdigKey.*.credentials, [""])[0]
+  value       = concat(ibm_resource_key.sysdig_key.*.credentials, [""])[0]
 }
