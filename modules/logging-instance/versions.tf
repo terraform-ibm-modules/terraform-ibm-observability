548a1f94bd7593e5c6cae7bc03a2d9d3136d2cf3
Todd Giguere <todd.giguere@ibm.com>
2022-06-27 09:08:13 -0400

    chore: change tf required version in submodules to match parent

--- modules/logging-instance/versions.tf
+++ modules/logging-instance/versions.tf
@@ -19,7 +19,7 @@ If we dont configure the version parameter, it fetched latest provider version.
 ****************************************************/
 
 terraform {
-  required_version = ">=0.13"
+  required_version = ">= 1.0.0"
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
