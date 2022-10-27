c3be09657606ab54cc485624f4eae8b782b33c4e
Andrej <akocbek@ie.ibm.com>
2022-10-27 16:40:38 +0100

    fix: address tflint terraform_deprecated_index rule

--- modules/activity-tracker-instance/outputs.tf
+++ modules/activity-tracker-instance/outputs.tf
@@ -5,25 +5,25 @@
 
 output "id" {
   description = "ID of Activity Tracker ATS"
-  value       = var.provision ? concat(ibm_resource_instance.activity_tracker.*.id, [""])[0] : concat(data.ibm_resource_instance.activity_tracker.*.id, [""])[0]
+  value       = var.provision ? concat(ibm_resource_instance.activity_tracker[*].id, [""])[0] : concat(data.ibm_resource_instance.activity_tracker[*].id, [""])[0]
 }
 
 output "guid" {
   description = "The GUID of the activity tracker"
-  value       = var.provision ? concat(ibm_resource_instance.activity_tracker.*.guid, [""])[0] : concat(data.ibm_resource_instance.activity_tracker.*.guid, [""])[0]
+  value       = var.provision ? concat(ibm_resource_instance.activity_tracker[*].guid, [""])[0] : concat(data.ibm_resource_instance.activity_tracker[*].guid, [""])[0]
 }
 
 output "key_id" {
   description = "Activity Tracker ATS key id"
-  value       = concat(ibm_resource_key.activity_tracker_key.*.id, [""])[0]
+  value       = concat(ibm_resource_key.activity_tracker_key[*].id, [""])[0]
 }
 
 output "key_guid" {
   description = "Activity Tracker ATS key guid"
-  value       = concat(ibm_resource_key.activity_tracker_key.*.guid, [""])[0]
+  value       = concat(ibm_resource_key.activity_tracker_key[*].guid, [""])[0]
 }
 
 output "key_credentials" {
   description = "Activity Tracker ATS key credentials"
-  value       = concat(ibm_resource_key.activity_tracker_key.*.credentials, [""])[0]
+  value       = concat(ibm_resource_key.activity_tracker_key[*].credentials, [""])[0]
 }
--- modules/monitoring-sysdig/outputs.tf
+++ modules/monitoring-sysdig/outputs.tf
@@ -5,25 +5,25 @@
 
 output "id" {
   description = "The ID of the sysdig instance"
-  value       = var.provision ? concat(ibm_resource_instance.sysdig_instance.*.id, [""])[0] : concat(data.ibm_resource_instance.sysdig.*.id, [""])[0]
+  value       = var.provision ? concat(ibm_resource_instance.sysdig_instance[*].id, [""])[0] : concat(data.ibm_resource_instance.sysdig[*].id, [""])[0]
 }
 
 output "guid" {
   description = "The GUID of the sysdig instance"
-  value       = var.provision ? concat(ibm_resource_instance.sysdig_instance.*.guid, [""])[0] : concat(data.ibm_resource_instance.sysdig.*.guid, [""])[0]
+  value       = var.provision ? concat(ibm_resource_instance.sysdig_instance[*].guid, [""])[0] : concat(data.ibm_resource_instance.sysdig[*].guid, [""])[0]
 }
 
 output "key_id" {
   description = "ID of the sysdig instance key"
-  value       = concat(ibm_resource_key.sysdig_key.*.id, [""])[0]
+  value       = concat(ibm_resource_key.sysdig_key[*].id, [""])[0]
 }
 
 output "key_guid" {
   description = "sysdig key guid"
-  value       = concat(ibm_resource_key.sysdig_key.*.guid, [""])[0]
+  value       = concat(ibm_resource_key.sysdig_key[*].guid, [""])[0]
 }
 
 output "key_credentials" {
   description = "sysdig key credentials"
-  value       = concat(ibm_resource_key.sysdig_key.*.credentials, [""])[0]
+  value       = concat(ibm_resource_key.sysdig_key[*].credentials, [""])[0]
 }
