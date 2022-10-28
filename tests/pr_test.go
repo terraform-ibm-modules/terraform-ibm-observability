fdfa9ddfc53d0eadc87e712c1fb346ff718a7fe4
Todd Giguere <todd.giguere@ibm.com>
2022-10-28 11:08:28 -0400

    test: use new activity region selection feature for parallel tests

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -2,14 +2,28 @@ package test
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/cloudinfo"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
 )
 
 const basicExampleTerraformDir = "examples/observability-complete"
 const resourceGroup = "geretain-test-observability-instances"
+
+var sharedInfoSvc *cloudinfo.CloudInfoService
+
+// TestMain will be run before any parallel tests, used to set up a shared InfoService object to track region usage
+// for multiple tests.
+// There is new feature in terratest-wrapper that will keep Activity Tracker tests on unique regions to avoid conflicts,
+// and using a shared InfoService will allow this feature to work between multiple tests running in parallel.
+func TestMain(m *testing.M) {
+	sharedInfoSvc, _ = cloudinfo.NewCloudInfoServiceFromEnv("TF_VAR_ibmcloud_api_key", cloudinfo.CloudInfoServiceOptions{})
+
+	os.Exit(m.Run())
+}
 
 /*
 *********************************************
@@ -20,15 +34,26 @@ If you have a test that uses a different set of options you can set those
 up in the individual test.
 ***********************************************
 */
-func setupOptions(t *testing.T, prefix string, region1 string, region2 string) *testhelper.TestOptions {
+func setupOptions(t *testing.T, prefix string) *testhelper.TestOptions {
 
 	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
-		Testing:       t,
-		TerraformDir:  basicExampleTerraformDir,
-		Prefix:        prefix,
-		ResourceGroup: resourceGroup,
-		Region:        region1,
+		Testing:                       t,
+		TerraformDir:                  basicExampleTerraformDir,
+		Prefix:                        prefix,
+		ResourceGroup:                 resourceGroup,
+		ExcludeActivityTrackerRegions: true,          // do not select regions that already have an AT
+		CloudInfoService:              sharedInfoSvc, // use pointer to shared info svc to keep track of region seleections
+		DefaultRegion:                 "us-south",
 	})
+
+	// default options will take care of selecting a good first region, but this test needs a 2nd region for super tenents.
+	// this extra block will select a 2nd region, and due to using a shared Info Service it will select a different region than
+	// the default options selection, or any other parallel test runs.
+	regionOptions := &testhelper.TesthelperTerraformOptions{
+		ExcludeActivityTrackerRegions: true,          // do not select regions that already have an AT
+		CloudInfoService:              sharedInfoSvc, // use pointer to shared info svc to keep track of region seleections
+	}
+	region2, _ := testhelper.GetBestVpcRegionO(options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], "../common-dev-assets/common-go-assets/cloudinfo-region-vpc-gen2-prefs.yaml", "us-east", *regionOptions)
 
 	options.TerraformVars = map[string]interface{}{
 		"logging_name":                       fmt.Sprintf("%s-%s", options.Prefix, "logging"),
@@ -57,7 +82,7 @@ if there are no further changes to apply (idempotent check).
 func TestRunBasicExample(t *testing.T) {
 	t.Parallel()
 
-	options := setupOptions(t, "ibm-obs-all", "eu-gb", "eu-de")
+	options := setupOptions(t, "ibm-obs-all")
 
 	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
@@ -83,7 +108,7 @@ the "BREAKING CHANGE" text.
 func TestRunUpgradeExample(t *testing.T) {
 	t.Parallel()
 
-	options := setupOptions(t, "ibm-obs-upg", "us-east", "ca-tor")
+	options := setupOptions(t, "ibm-obs-upg")
 
 	output, err := options.RunTestUpgrade()
 	if !options.UpgradeTestSkipped {
